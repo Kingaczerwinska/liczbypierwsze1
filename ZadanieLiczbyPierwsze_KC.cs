c6a484d08ed8d27105d51cd1350e3a82c7953339
Kingaczerwinska <91785829+Kingaczerwinska@users.noreply.github.com>
2021-10-29 23:14:13 +0200

    Add files via upload

---
+++ ZadanieLiczbyPierwsze_KC.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace liczby_pierwsze
+{
+    class Program
+    {
+        static bool czypierwsze(int liczba)
+        {
+            if (liczba == 2)
+            {
+                return true;
+            }
+            if (liczba <= 1)
+            {
+                return false;
+            }
+            else
+            {
+                for (int i = 2; i < liczba; i++)
+                {
+                    if (liczba % i == 0)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("ile liczb pierwszych chcesz wypisać? ");
+            int liczbyPierwsze = Convert.ToInt32(Console.ReadLine());
+
+            int j = 1;
+            int i = 1;
+            while (j <= liczbyPierwsze)
+            {
+                if (czypierwsze(i))
+                {
+                    Console.WriteLine(i);
+                    j++;
+                }
+                i++;
+            }
+        }
+    }
+}
+    
+
